9e1a41ac1919b343e500f350b09148263bb0262e
Aiandra  Meira <33138081+aiandralves@users.noreply.github.com>
2026-07-27 22:41:30 -0300

    🚧 chore: simplify footer component and update social links

--- apps/web/src/app/presentation/views/footer/footer.ts
+++ apps/web/src/app/presentation/views/footer/footer.ts
@@ -1,20 +1,17 @@
-import { AiBadge, AiButton, AiSeparator } from "@aiandralves/ai-ui";
+import { AiButton } from "@aiandralves/ai-ui";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { environment } from "@env/environment.development";
 
 @Component({
     selector: "doc-footer",
-    imports: [AiButton, AiSeparator, AiBadge],
+    imports: [AiButton],
     templateUrl: "./footer.html",
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DocFooter {
-    protected readonly message = "Feito com ❤️ por @aiandrameira. Código aberto";
-    protected readonly copyright = "© 2026 Aiandra Alves";
+    protected readonly message = "Feito com ❤️ por @aiandralves. Código aberto | © 2026.";
     protected readonly social = {
-        github: "https://github.com/aiandrameira/ai-docs",
+        github: "https://github.com/aiandrameira",
         linkedin: "https://www.linkedin.com/in/aiandralves/",
-        npnpmjs: "https://www.npmjs.com/package/@aiandralves/ai-ui",
+        npm: "https://www.npmjs.com/package/@aiandralves/ai-docs",
     };
-    protected version = environment.version;
 }
